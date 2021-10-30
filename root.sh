8621b9fe3ceafafb62f9754a44a70acd2c2eb119
shyam <shyam055119@gmail.com>
2021-10-30 11:48:43 +0200

    my files

--- root.sh
+++ root.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-source /cvmfs/eic.opensciencegrid.org/default/opt/fun4all/core/bin/eic_setup.sh -n
+source /cvmfs/eic.opensciencegrid.org/default/opt/fun4all/core/bin/eic_setup.sh -n   # setup EIC environment in the singularity container shell. Note the shell is bash by default
--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 singularity shell -B cvmfs:/cvmfs cvmfs/eic.opensciencegrid.org/singularity/rhic_sl7_ext.simg
 
+
+
