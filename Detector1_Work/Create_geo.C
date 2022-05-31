fb8c0bac71d235a7d6afecccc73c9fe6fb6b209f
Shyam <shyam.kumar@uniba.it>
2022-05-31 12:24:32 +0200

    fix

--- Detector1_Work/Create_geo.C
+++ Detector1_Work/Create_geo.C
@@ -7,8 +7,12 @@ void Create_geo()
 
    TObjArray* volumes = gGeoManager->GetListOfVolumes();
   
-    for(Int_t i=0; i<volumes->GetEntries();++i)
+    const Int_t nvolumes = volumes->GetEntries();
+    TCanvas *c[nvolumes]; 
+    
+    for(Int_t i=0; i<5;++i)
     {
+    
       TGeoVolume* vol= (TGeoVolume*) volumes->At(i);
       TString volname = vol->GetName();
       //   if (volname.Contains("SVTX")) vol->SetLineColor(kRed); 	
@@ -29,6 +33,29 @@ void Create_geo()
      Double_t *cent; cent = center;
      viewer->SetOrthoCamera(TGLViewer::kCameraOrthoZOX,10.0, 1.0, cent,1.0,0.); // kCameraPerspXOZ, kCameraPerspYOZ, kCameraPerspXOY, kCameraOrthoXOY,
      viewer->SavePicture(Form("%s.png",volname.Data()));
+    
+    c[i] = new TCanvas(Form("radlen_%d",i), "radiation length");
+	  c[i]->SetRightMargin(0.18); 
+    TH2F *X0 = gGeoManager->GetVolume(i)->LegoPlot(180,0.,180.,360,0.,360.);
+    X0->SetTitle(Form("%s",volname.Data()));
+    X0->SetXTitle( "#varphi (deg)");
+    X0->SetYTitle( "#theta (deg)");
+    X0->SetZTitle( "X/X0");
+    X0->GetXaxis()->SetRangeUser(0.,360.);
+    X0->GetZaxis()->SetTitleOffset(1.4);
+	  gStyle->SetPalette(kInvertedDarkBodyRadiator);
+    X0->SetStats(kFALSE);
+    X0->Draw("colz");
+    c[i]->Update();
+    TPaletteAxis *palette = (TPaletteAxis*)X0->GetListOfFunctions()->FindObject("palette");
+   double y1 = palette->GetY1NDC();
+   double y2 = palette->GetY2NDC();
+   double x1 = palette->GetX1NDC();
+   double x2 = palette->GetX2NDC();
+   palette->SetX1NDC(x1+0.02);
+   palette->SetX2NDC(x2+0.02);
+   c[i]->Print(Form("MB_%s.png",volname.Data()));
+     
    //kCameraOrthoXOZ, kCameraOrthoZOY, kCameraOrthoXnOY, kCameraOrthoXnOZ,kCameraOrthoZnOY
   // v->CurrentCamera().RotateRad(-.7, 0.5);
          
